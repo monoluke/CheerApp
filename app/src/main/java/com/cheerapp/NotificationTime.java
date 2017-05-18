8841e6f89833c5c177edca244b080fe5d6dc1f44
eranshpi <eran.shpigelman@mail.huji.ac.il>
2017-05-19 00:43:56 +0300

    add class "eran"

--- app/src/main/java/com/cheerapp/NotificationTime.java
+++ app/src/main/java/com/cheerapp/NotificationTime.java
@@ -1,26 +1,29 @@
 package com.cheerapp;
-import android.support.annotation.IntDef;
 
 import com.cheerapp.Categories.Category;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
-import static java.lang.System.in;
 
 /**
  * Created by User on 18/05/2017.
  */
 
 public class NotificationTime {
-    public static Boolean determineTime(Category category, Calendar time) {
+    static Boolean determineTime(Category category, Calendar time) {
         Calendar now = new GregorianCalendar();
-
-
-        for (int k = 0; k < category.messages.length; k++) {
-
-
+        long differentInMilli = time.getTime().getTime() - now.getTime().getTime();
+        long differentInSec = TimeUnit.MILLISECONDS.toMinutes(differentInMilli);
+        for (long a : category.notificationTimes)
+        {
+            if(a == differentInSec){
+                return true;
+            }
         }
+        return false;
     }
 
 }
