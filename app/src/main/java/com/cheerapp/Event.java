00c2c4e816d9ce5616a2f9d6f9362ce09a890532
hadas <hadasjacobi@gmail.com>
2017-05-18 19:56:50 +0300

    event parsing

---
+++ app/src/main/java/com/cheerapp/Event.java
@@ -0,0 +1,13 @@
+package com.cheerapp;
+
+import java.util.Calendar;
+import java.util.Calendar;
+
+/**
+ * Created by hadas on 18-May-17.
+ */
+
+public class Event {
+    public String description;
+    public Calendar startTime;
+}
---
+++ app/src/main/java/com/cheerapp/eventParser.java
@@ -0,0 +1,11 @@
+package com.cheerapp;
+
+/**
+ * Created by hadas on 18-May-17.
+ */
+
+public class eventParser {
+    public static void makeNotifications() {
+
+    }
+}
