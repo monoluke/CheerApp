4313deea54615a4e91012e0175895701eccb872a
hadas <hadasjacobi@gmail.com>
2017-05-18 23:00:07 +0300

    event parsing

--- app/src/main/java/com/cheerapp/Event.java
+++ app/src/main/java/com/cheerapp/Event.java
@@ -1,13 +1,20 @@
 package com.cheerapp;
 
 import java.util.Calendar;
-import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
 
 /**
  * Created by hadas on 18-May-17.
  */
 
 public class Event {
+
     public String description;
     public Calendar startTime;
+
+    public Event(String description, Calendar startTime) {
+        this.description = description;
+        this.startTime = startTime;
+    }
 }
