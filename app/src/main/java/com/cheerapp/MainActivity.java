87da657d31cfbf83a9ff1e96e418dea8e67980ea
shai <shaimaarek@gmail.com>
2017-05-18 22:41:16 +0300

    test

--- app/src/main/java/com/cheerapp/MainActivity.java
+++ app/src/main/java/com/cheerapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cheerapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
