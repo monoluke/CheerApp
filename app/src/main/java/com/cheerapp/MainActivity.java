aaae8274abd306ea612d2943b8b10d7ddc76d76e
shai <shaimaarek@gmail.com>
2017-05-18 23:54:21 +0300

    Updated CheerApp logo in MainActivity and colors.xml in res->layout->values

--- app/src/main/java/com/cheerapp/MainActivity.java
+++ app/src/main/java/com/cheerapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 
 import java.util.ArrayList;
 
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Classifier c = new Classifier();
-        c.categoryDetector("TEST EXAM VACATION GALA and in DInner");
+
 
 
         //ContentResolver contentResolver = this.getContentResolver();
