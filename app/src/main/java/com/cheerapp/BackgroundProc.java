ad783d45115eb0a46a30fb74c4b808e7be642260
shai <shaimaarek@gmail.com>
2017-05-19 09:00:05 +0300

    Small fixes.

--- app/src/main/java/com/cheerapp/BackgroundProc.java
+++ app/src/main/java/com/cheerapp/BackgroundProc.java
@@ -51,7 +51,7 @@ public class BackgroundProc extends IntentService {
         while (true)
         {
             try {
-                TimeUnit.SECONDS.sleep(10);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
